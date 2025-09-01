3c873d3e2879433d09d9ae8e1875151726e93868
Albert Williams W <albertwilliamsw@Alberts-MacBook-Pro.local>
2025-09-01 16:35:33 +0530

    added hobbies component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component";
+import { HobbiesPageComponent } from './hobbies-page/hobbies-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent, HobbiesPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/hobbies-page/hobbies-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobbies-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './hobbies-page.component.html',
+  styleUrl: './hobbies-page.component.scss'
+})
+export class HobbiesPageComponent {
+
+}
