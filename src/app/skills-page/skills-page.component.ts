940d35bee316037db76c1b729e10fb59e37fedc4
Albert Williams W <albertwilliamsw@Alberts-MacBook-Pro.local>
2025-09-02 16:24:33 +0530

    added commonmodule to skills page

--- src/app/skills-page/skills-page.component.ts
+++ src/app/skills-page/skills-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, NgZone, Output } from '@angular/core';
 
 @Component({
   selector: 'app-skills-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills-page.component.html',
   styleUrl: './skills-page.component.scss'
 })
